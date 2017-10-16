539e091e34dc457a7e1f0745eae2c07bbcee9681
Zach LeBlanc <zachleb112@gmail.com>
2017-10-16 14:32:31 -0400

    Added files to run ios and android

---
+++ DailyLogger/platforms/ios/build/DailyLogger.build/Debug-iphonesimulator/DailyLogger.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/zachleblanc/Projects/NativeScript/Comp490App/dailylogger/DailyLogger/platforms/ios/DailyLogger/DailyLogger-Prefix.pch \
+  /Users/zachleblanc/Projects/NativeScript/Comp490App/dailylogger/DailyLogger/platforms/ios/internal/main.m \
+  /Users/zachleblanc/Projects/NativeScript/Comp490App/dailylogger/DailyLogger/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/zachleblanc/Projects/NativeScript/Comp490App/dailylogger/DailyLogger/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/zachleblanc/Projects/NativeScript/Comp490App/dailylogger/DailyLogger/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/zachleblanc/Projects/NativeScript/Comp490App/dailylogger/DailyLogger/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/zachleblanc/Projects/NativeScript/Comp490App/dailylogger/DailyLogger/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/zachleblanc/Projects/NativeScript/Comp490App/dailylogger/DailyLogger/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/zachleblanc/Projects/NativeScript/Comp490App/dailylogger/DailyLogger/platforms/ios/internal/TNSDebugging.h \
+  /Users/zachleblanc/Projects/NativeScript/Comp490App/dailylogger/DailyLogger/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
