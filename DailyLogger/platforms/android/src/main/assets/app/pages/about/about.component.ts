964353f57b0fe96ef2d665bb0fa02674522b70d0
AMRoody <annaroodbergen@yahoo.com>
2017-11-18 21:11:26 -0500

    Tabs
    Tabs at top of about page, can't click to go to another page

--- DailyLogger/platforms/android/src/main/assets/app/pages/about/about.component.ts
+++ DailyLogger/platforms/android/src/main/assets/app/pages/about/about.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { TextView } from "ui/text-view";
 import { isAndroid } from "platform";
 import { RouterExtensions } from "nativescript-angular/router";
+import { TabView, SelectedIndexChangedEventData } from "ui/tab-view";
 
 @Component({
   selector: "list",
@@ -9,11 +10,15 @@ import { RouterExtensions } from "nativescript-angular/router";
   templateUrl: "./about.html",
   styleUrls: ["./about-common.css"]
 })
+
 export class AboutComponent {
   constructor(private routerExtensions: RouterExtensions) {
 
- }
+  }
+
   about() {
     this.routerExtensions.navigate(["/about"]);
   }
 }
+
+//Code adapted from NativeScript and Angular tutorial for tabs
