1caba7078cea40319b4974dc3b2a16224b4bbce5
AMRoody <annaroodbergen@yahoo.com>
2017-11-19 19:55:54 -0500

    updates

--- DailyLogger/app/app.routing.ts
+++ DailyLogger/app/app.routing.ts
@@ -2,14 +2,16 @@
 import { LoginComponent } from "./pages/login/login.component";
 import { ListComponent } from "./pages/list/list.component";
 import { AboutComponent } from "./pages/about/about.component";
-import {TabComponent} from "./pages/tab/tab.component"
+import {TabComponent} from "./pages/tab/tab.component";
+import {ProfileComponent} from "./pages/profile/profile.component"
 
 // Make route by adding path and component
 export const routes = [
   { path: "", component: LoginComponent },
   { path: "list", component: ListComponent },
   { path: "about", component: AboutComponent },
-  { path: "tab", component: TabComponent}
+  { path: "tab", component: TabComponent},
+  { path: "profile", component: ProfileComponent}
 ];
 
 // Add component to this list.
@@ -17,5 +19,6 @@ export const navigatableComponents = [
   LoginComponent,
   ListComponent,
   AboutComponent,
-  TabComponent
+  TabComponent,
+  ProfileComponent
 ];
